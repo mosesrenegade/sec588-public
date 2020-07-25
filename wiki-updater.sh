c3e89190c827e4aad0643522951d5ded6046aa49
Moses Frost <moses@moses.io>
2020-07-25 17:57:56 -0400

    Working fix for wiki-update.sh

--- wiki-updater.sh
+++ wiki-updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function UPDATE_WIKI () {\
+function UPDATE_WIKI () {
     cp ./wiki-updater.sh /wiki-updater.old
     curl -o ./wiki-updater.sh https://raw.githubusercontent.com/mosesrenegade/sec588-public/master/wiki-updater.sh
     echo "You may have to run this application two times to update the shell script"
@@ -10,6 +10,10 @@ function UPDATE_WIKI () {\
     sudo cp -r . /var/www/html/wiki
 }
 
+function UPDATE_ENV() {
+    c
+
+}
 function HELP () {
     echo "This course features a quest to update this wiki, you must"
     echo "pass lab 1.5 to update your local wiki! Have fun exploring"
@@ -18,7 +22,7 @@ function HELP () {
 echo "Have you done Lab 1.5 yet?"
 select yn in "Yes" "No"; do
     case $yn in
-        [Yy]* ) UPDATE_WIKI; break;;
+        [Yy]* ) UPDATE_WIKI;UPDATE_ENV;break;;
         [Nn]* ) HELP; exit;;
         * ) "Please answer yes or no.";;
     esac
