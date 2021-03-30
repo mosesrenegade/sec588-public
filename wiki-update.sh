47c2e3d479ffc8696d1792a50ea68d16fafff65e
mosesrenegade <moses@moses.io>
2021-03-30 13:23:39 -0400

    Update wiki-update.sh

--- wiki-update.sh
+++ wiki-update.sh
@@ -31,7 +31,7 @@ function UPDATE_WIKI () {
 
 function UPDATE_ENV() {
     FILE="/home/sec588/.bashrc"
-    UA='"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'"
+    UA='"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"'
     if grep -q "CLASS" "$FILE"
     then
         sed -i '/CLASS=/d' $FILE
