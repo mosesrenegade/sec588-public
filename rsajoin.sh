e6667de148425a3087e67342c24d012d21c5bad2
mosesrenegade <moses@moses.io>
2021-04-23 10:48:22 -0400

    rsajoin

---
+++ rsajoin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -Rf /home/sec588/Coursefiles/workdir/foundssh-key-online
+sed -e "s/-----BEGIN OPENSSH PRIVATE KEY-----/&\n/"\
+    -e "s/-----END OPENSSH PRIVATE KEY-----/\n&/"\
+    -e "s/\S\{64\}/&\n/g"\
+    /home/sec588/Coursefiles/workdir/foundssh-key-oneline > /home/sec588/Coursefiles/workdir/foundssh-key
+chmod 600 /home/sec588/Coursesiles/workdir/foundssh-key
+
