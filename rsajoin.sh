65386bcf20964406e46533119ff0cd8ca27b1996
mosesrenegade <moses@moses.io>
2021-04-23 11:11:09 -0400

    Test

--- rsajoin.sh
+++ rsajoin.sh
@@ -4,5 +4,5 @@ sed -e "s/-----BEGIN OPENSSH PRIVATE KEY-----/&\n/"\
     -e "s/-----END OPENSSH PRIVATE KEY-----/\n&/"\
     -e "s/\S\{64\}/&\n/g"\
     /home/sec588/Coursefiles/workdir/foundssh-key-oneline > /home/sec588/Coursefiles/workdir/foundssh-key
-chmod 600 /home/sec588/Coursesiles/workdir/foundssh-key
+chmod 600 /home/sec588/Coursefiles/workdir/foundssh-key
 
