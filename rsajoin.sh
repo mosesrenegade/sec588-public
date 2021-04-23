2c8670d96742478cfa3c75d34eeabed9ecc17076
mosesrenegade <moses@moses.io>
2021-04-23 10:53:49 -0400

    Test

--- rsajoin.sh
+++ rsajoin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -Rf /home/sec588/Coursefiles/workdir/foundssh-key-online
+rm -Rf /home/sec588/Coursefiles/workdir/foundssh-key
 sed -e "s/-----BEGIN OPENSSH PRIVATE KEY-----/&\n/"\
     -e "s/-----END OPENSSH PRIVATE KEY-----/\n&/"\
     -e "s/\S\{64\}/&\n/g"\
